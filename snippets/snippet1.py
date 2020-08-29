f842b2a03b3243ce9eb01003f497a575a8b35cbc
Wolfgang Hottgenroth <wolfgang.hottgenroth@icloud.com>
2020-08-29 13:42:39 +0200

    dumb test fix 1

--- snippets/snippet1.py
+++ snippets/snippet1.py
@@ -135,7 +135,7 @@ class MeterbusSerial(object):
       print("Waiting for input ... ")
       c = self.port.read()
 
-      print("State {}, Octet {:02X}".format(state, c))
+      print("State {}, Octet {:02X}".format(state, c[0]))
 
 
 
